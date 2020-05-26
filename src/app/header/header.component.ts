7c2ce5ee6ebfcc6c7c97befb94846c6d75941fc3
HemanthK9 <43072512+HemanthK9@users.noreply.github.com>
2020-05-27 00:21:38 +0530

    added routing

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,20 +1,10 @@
-import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-  @Output() linkEmitter = new EventEmitter<string>();
-  linkClicked: string;
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
-  onLinkClicked() {
-    this.linkEmitter.emit(this.linkClicked);
-  }
+export class HeaderComponent {
+  
 }
